221d6fc002ad1f9d0314e1746a25d05fd1cc1cfd
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-01-09 00:46:39 +0530

    Simple rock paper scissor game is working

---
+++ script.js
@@ -0,0 +1,54 @@
+const moves = ['rock', 'paper', 'scissor'];
+const resultDiv = document.getElementById('result');
+const playerScoreDiv = document.getElementById('player_score');
+const computerScoreDiv = document.getElementById('computer_score');
+
+let playerWinCount = 0;
+let computerWinCount = 0;
+
+const winningComditions = [
+  ['rock', 'scissor'],
+  ['papper', 'rock'],
+  ['scissor', 'papper']
+];
+
+const hasWon = function(computerMove, playersMove) {
+  console.log(computerMove, playersMove);
+  return winningComditions.some(condition => {
+    console.log(playersMove == condition[0] && computerMove == condition[1]);
+    return computerMove == condition[0] && playersMove == condition[1];
+  });
+};
+
+const randomGenerator = function() {
+  return Math.floor(Math.random() * 3);
+};
+
+const getComputerMove = function() {
+  const index = randomGenerator();
+  return moves[index];
+};
+
+const setPlayerMove = function(event) {
+  const playerMove = event.target.id;
+  const computerMove = getComputerMove();
+  checkWinConditions(computerMove, playerMove);
+  updateScore();
+};
+
+const updateScore = function() {
+  playerScoreDiv.innerText = playerWinCount;
+  computerScoreDiv.innerText = computerWinCount;
+};
+
+const checkWinConditions = function(computerMove, playerMove) {
+  if (computerMove == playerMove) {
+    return (resultDiv.innerText = `Your move ${playerMove} Computer's move ${computerMove} Draw!`);
+  }
+  if (hasWon(computerMove, playerMove)) {
+    computerWinCount++;
+    return (resultDiv.innerText = `Your move ${playerMove} Computer's move ${computerMove} Computer Won!`);
+  }
+  playerWinCount++;
+  return (resultDiv.innerText = `Your move ${playerMove} Computer's move ${computerMove} You Won!`);
+};
